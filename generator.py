7663e87d4a881669a80c1a3bfeb7caa2a2218609
THESHADOW2030 <hazem030500@gmail.com>
2023-08-16 21:02:29 +0200

    generator with up512

---
+++ generator.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn as nn
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, inChannels, outChannels, down=True, use_act = True, **kwargs):
+        super(ConvBlock, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(inChannels, outChannels, **kwargs) if down else nn.ConvTranspose2d(inChannels, outChannels, **kwargs),
+            nn.InstanceNorm2d(outChannels), 
+            nn.ReLU(inplace=True) if use_act else nn.Identity()
+        )
+    
+
+    def forward(self, x):
+        return self.conv(x)
+    
+
+class ResidualBlock(nn.Module):
+    def __init__(self, channcels):
+        super(ResidualBlock, self).__init__()
+
+        self.block = nn.Sequential(ConvBlock(channcels, channcels, kernel_size = 3, padding = 1),
+                                   ConvBlock(channcels, channcels, use_act = False, kernel_size = 3, padding = 1)
+                                    )
+        
+    def forward(self, x):
+        return x + self.block(x)
+    
+
+class Generator(nn.Module):
+    def __init__(self, imgChannels, numFeatures = 64, numResiduals = 9, finalSize = 512):
+        super(Generator, self).__init__()
+
+        self.initial = nn.Sequential(
+            nn.Conv2d(imgChannels, numFeatures, kernel_size=7, stride=1, padding=3, padding_mode="reflect"),
+            nn.ReLU(inplace=True)
+        )
+
+        self.downBlocks = nn.ModuleList([   #for downsampling
+            ConvBlock(numFeatures, numFeatures*2, kernel_size = 3, stride = 2, padding = 1),
+            ConvBlock(numFeatures*2, numFeatures*4,kernel_size = 3, stride = 2, padding = 1 )
+            
+        ])
+
+        self.residualBlocks = nn.Sequential(*[ResidualBlock(numFeatures*4) for _ in range(numResiduals)])
+
+        self.convBlock512 = ConvBlock(numFeatures*4, numFeatures*8, kernel_size = 3, stride = 1, padding = 1) 
+
+        
+
+        #upsampling to double the size (finalSize)
+        self.upBlocks = nn.ModuleList([
+            ConvBlock(numFeatures*8, numFeatures*4, down=False, kernel_size = 3, stride = 2, padding = 1, output_padding = 1),
+            ConvBlock(numFeatures*4, numFeatures * 2, down=False, kernel_size = 3, stride = 2, padding = 1, output_padding = 1),
+            ConvBlock(numFeatures*2, numFeatures, down=False, kernel_size = 3, stride = 2, padding = 1, output_padding = 1)
+        ])
+
+        self.last = nn.Conv2d(numFeatures, imgChannels, kernel_size=7, stride=1, padding=3, padding_mode="reflect")
+        
+        
+
+    def forward(self, x):
+        print("Input: ", x.shape)
+        x = self.initial(x)
+        print("After initial: ", x.shape)
+        for layer in self.downBlocks:
+            x = layer(x)
+            print("After down: ", x.shape)
+        x = self.residualBlocks(x)
+        print("After residual: ", x.shape)
+        x = self.convBlock512(x)
+        print("After convBlock512: ", x.shape)
+        for layer in self.upBlocks:
+            x = layer(x)
+            print("After up: ", x.shape)
+        x = self.last(x)
+        print("After last: ", x.shape)
+        return torch.tanh(x)
+    
+    
+
+
+
+def test():
+    imgChannels = 3
+    imgSize = 256
+    x = torch.randn((2, imgChannels, imgSize, imgSize))
+    gen  = Generator(imgChannels, numFeatures=64, numResiduals=9, finalSize=256)
+   
+    print(gen(x).shape)
+
+
+if __name__ == "__main__":
+    test()
