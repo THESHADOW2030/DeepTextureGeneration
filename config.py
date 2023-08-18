c906a3ababcf36ee3ca29d4de5da491ec41a44b3
THESHADOW2030 <hazem030500@gmail.com>
2023-08-18 19:32:23 +0200

    added more model classes

--- config.py
+++ config.py
@@ -1,19 +1,27 @@
 class weightsName:
     GENERAL = {
             "generator" : "./weights/generalGeneratorWeights.pth.tar", 
-            "discriminator" : "./weights/generalDiscriminatorWeights.pth.tar"
+            "discriminator" : "./weights/generalDiscriminatorWeights.pth.tar",
+            "dataName" : "general"
         }
     GENERAL_STYLE = {
             "generator" : "./weights/generalStyleGeneratorWeights.pth.tar",
-            "discriminator" : "./weights/generalStyleDiscriminatorWeights.pth.tar"
+            "discriminator" : "./weights/generalStyleDiscriminatorWeights.pth.tar",
+            "dataName" : "general"
+        }
+    
+    bubbly_SPECIALIZED= {
+            "generator" : "./weights/bubblyGeneratorWeights.pth.tar",
+            "discriminator" : "./weights/bubblyDiscriminatorWeights.pth.tar",
+            "dataName" : "bubbly" 
+        }
+    fibrous_SPECIALIZED= {
+            "generator" : "./weights/fibrousGeneratorWeights.pth.tar",
+            "discriminator" : "./weights/fibrousDiscriminatorWeights.pth.tar",
+            "dataName" : "fibrous"
         }
     
     
-class trainingData:
-    mode = {
-            "full" : "full",
-            "subset" : "subset"
-    }
 
 
 learningRate = 2e-4
